4ef5a0da4ebdaff3ba7ad88c557c59a4d889dd00
joaquinabal <joabal97@gmail.com>
2023-05-23 22:00:41 -0300

    Modificacion Punto 1 - Avance en Punto 2

--- funciones.py
+++ funciones.py
@@ -14,12 +14,13 @@ def leer_archivo(ruta:str):
         diccionario = json.load(archivo)
     return diccionario["jugadores"]
 
-def mostrar_jugadores_dreamteam(lista_original: list[dict]):
+def mostrar_jugadores_dreamteam(lista_original: list[dict], flag_indice: bool):
     '''
     Esta función muestra un listado de jugadores, con su nombre y posición.
     ---------
     Parámetros:
     lista_original: tipo list[dict] -> la lista original que se importó del JSON.
+    flag_indice: tipo bool -> una flag que permite mostrar, o no, el indice correspndiente al jugador.
     ---------
     Devuelve:
     False: en caso de que lista_original se encuentre vacía.
@@ -29,8 +30,11 @@ def mostrar_jugadores_dreamteam(lista_original: list[dict]):
         return False
     lista_jugadores = lista_original[:]
     lista_jugadores_nombre_pos = []
-    for jugador in lista_jugadores:
-        lista_jugadores_nombre_pos.append([jugador["nombre"], jugador["posicion"]])
+    for indice in range(len(lista_jugadores) - 1):
+        lista_jugadores_nombre_pos.append([indice, lista_jugadores[indice]["nombre"], lista_jugadores[indice]["posicion"]])
     for jugador in lista_jugadores_nombre_pos:
-        mensaje = "{0} - {1}".format(jugador[0],jugador[1])
+        if not flag_indice:
+            mensaje = "{0} - {1}\n".format(jugador[1],jugador[2])
+        else:
+            mensaje = "{0} - {1} - {2}\n".format(jugador[0],jugador[1],jugador[2])   
         print(mensaje + "\n")
--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from funciones import (
     mostrar_jugadores_dreamteam
 )
 
-menu = "{0}\n{1}\n{2} ".format(
+menu = "{0}\n{1}\n{2}\n{3} ".format(
 "----------------------------------------------------------------",
 """1- Mostrar la lista de todos los jugadores del Dream Team. Con el formato:
 Nombre Jugador - Posición. Ejemplo:
 Michael Jordan - Escolta\n""",
+"""
+2- Permitir al usuario seleccionar un jugador por su índice y mostrar sus estadísticas completas, 
+incluyendo temporadas jugadas, puntos totales, promedio de puntos por partido, rebotes totales, 
+promedio de rebotes por partido, asistencias totales, promedio de asistencias por partido, robos totales, 
+bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
+""",
 "Ingrese la opción deseada" 
 )
 
@@ -21,4 +27,7 @@ while True:
     
     match(int(respuesta)):
         case 1:
-            mostrar_jugadores_dreamteam(lista_juegos_JSON)
+            mostrar_jugadores_dreamteam(lista_juegos_JSON, False)
+            
+        case 2:
+            mostrar_jugadores_dreamteam(lista_juegos_JSON, True)
