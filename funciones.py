9b9a27c43fbcc401c0b3cf203d147f54f98cf7ae
joaquinabal <joabal97@gmail.com>
2023-05-25 10:35:03 -0300

    Validación del menú, funciones para punto 3.

--- funciones.py
+++ funciones.py
@@ -39,7 +39,36 @@ def mostrar_jugadores_dreamteam(lista_original: list[dict], flag_indice: bool):
             mensaje = "{0} - {1} - {2}\n".format(jugador[0],jugador[1],jugador[2])   
         print(mensaje + "\n")
         
+def validacion_menu(numero) -> bool:
+    '''
+    Esta función busca validar el numero ingresado para que sea apto para el menú.(1-20,23)
+    -----------
+    Parámetros:
+    numero: tipo int -> el numero que se evaluará la validación.
+    -----------
+    Retorna:
+    True: En caso que haya coincidido el re.match.
+    False: En caso que no haya coincidido.
+    '''
+    validacion = re.match(r'[1]?[0-9]{1}$|20|23', numero)
+    if validacion:
+        return True
+    else:
+        return False
+                
+
 def mostrar_estadisticas(lista_original: list[dict], indice: int):
+    '''
+    Esta función muestra las estadísticas por consola del jugador elegído según su índice.
+    -----------
+    Parámetros:
+    lista_original: tipo list[dict] -> la lista original que se importó del JSON.
+    indice: tipo int -> el índice del jugador que se quiere seleccionar.
+    -----------
+    Retorna:
+    mensaje: tipo string -> el string que contiene las estadisticas del jugador.
+    '''
+    
     if not lista_original:
         print("La lista original se encuentra vacia.")
         return False
@@ -75,3 +104,67 @@ def mostrar_estadisticas(lista_original: list[dict], indice: int):
         lista_jugadores[indice]["nombre"]
     )
     return mensaje
+
+def generar_path_jugador(lista_original: list, indice: int, path: str):
+    '''
+    Esta función genera una ruta que incluye el nombre del jugador seleccionado por indice.
+    --------------
+    Parámetros:
+    lista_original: tipo list[dict] -> la lista original que se importó del JSON.
+    indice: tipo int -> el índice del jugador que se quiere seleccionar.
+    path: tipo string -> la ruta del directorio donde se quiere generar el path.
+    --------------
+    Retorna:
+    path_final: tipo string -> la ruta incluyendo el nombre del jugador.
+    '''
+    if len(lista_original) == 0:
+        print("Lista vacía.")
+        return -1   
+    lista = lista_original[:]
+    nombre_jugador = lista[indice]["nombre"]
+    path_final = path + nombre_jugador.replace(" ","_") + ".csv"
+    return path_final
+
+def guardar_csv_jugador_stats(lista_original: list, indice: int, path: str):
+    '''
+    Esta función genera un .csv de los datos y estadísticas del jugador seleccionado por índice.
+    ------------
+    lista_original: tipo list[dict] -> la lista original que se importó del JSON.
+    indice: tipo int -> el índice del jugador que se quiere seleccionar.
+    path: tipo string -> la ruta del directorio donde se quiere generar el archivo .csv.
+    ------------
+    Retorna:
+    No retorna nada.
+    '''
+    if len(lista_original) == 0:
+        print("Lista vacía.")
+        return -1
+    lista = lista_original[:]
+    lista_keys = ["nombre","posicion"]
+    lista_valores_stats = [lista[indice]["nombre"], lista[indice]["posicion"]]
+    lista_keys.extend(lista[indice]["estadisticas"].keys())
+    for value in lista[indice]["estadisticas"].values():
+        lista_valores_stats.append(str(value))
+    lista_valores_stats_string = ",".join(lista_valores_stats)
+    lista_keys_string = ",".join(lista_keys)
+    datos_para_csv = lista_keys_string + "\n" + lista_valores_stats_string
+    print(datos_para_csv)
+    with open(path, "w") as archivo:
+        archivo.writelines([datos_para_csv])
+    print("el archivo fue creado en {0}".format(path))
+
+def mostrar_logros_por_busqueda(lista_original: list[dict], nombre: str):
+    if len(lista_original) == 0:
+        print("Lista vacía.")
+        return -1
+    lista = lista_original[:]
+    flag_jugador = False
+    for jugador in lista:
+        busqueda = re.search(f'{nombre}', jugador["nombre"], re.I)
+        if busqueda:
+            flag_jugador = True
+            print(jugador["nombre"] + "\n")
+            for logro in jugador["logros"]:
+                print("- " + logro + "\n")       
+    if flag_jugador == False:
+        print("No existe jugador con ese nombre.")
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import re
+
 from funciones import (
     leer_archivo,
     mostrar_jugadores_dreamteam,
-    mostrar_estadisticas
+    mostrar_estadisticas,
+    validacion_menu,
+    guardar_csv_jugador_stats,
+    generar_path_jugador
 )
 
-menu = "{0}\n{1}\n{2}\n{3} ".format(
+menu = "{0}\n{1}\n{2}\n{3}\n{4} ".format(
 "----------------------------------------------------------------",
 """1- Mostrar la lista de todos los jugadores del Dream Team. Con el formato:
 Nombre Jugador - Posición. Ejemplo:
@@ -15,17 +20,27 @@ incluyendo temporadas jugadas, puntos totales, promedio de puntos por partido, r
 promedio de rebotes por partido, asistencias totales, promedio de asistencias por partido, robos totales, 
 bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
 """,
+"""
+3- Después de mostrar las estadísticas de un jugador seleccionado por el usuario, 
+permite al usuario guardar las estadísticas de ese jugador en un archivo CSV. 
+El archivo CSV debe contener los siguientes campos: nombre, posición, temporadas, 
+puntos totales, promedio de puntos por partido, rebotes totales, promedio de rebotes por partido, 
+asistencias totales, promedio de asistencias por partido, robos totales, bloqueos totales, 
+porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
+
+""",
 "Ingrese la opción deseada" 
 )
 
 path_JSON = "Parcial\dt.json"
-
+path_stats_jugadores = 'Parcial/stats_jugadores/'
+indice = None
 lista_dreamteam= leer_archivo(path_JSON)
-#print(lista_juegos_JSON)
 
 while True:
     respuesta = input(menu)
-    
+    while not respuesta.isnumeric() or validacion_menu(respuesta) == False:
+        respuesta = input("Opcion Invalida. Ingrese nuevamente: ")
     match(int(respuesta)):
         case 1:
             mostrar_jugadores_dreamteam(lista_dreamteam, False)
@@ -33,4 +48,17 @@ while True:
         case 2:
             mostrar_jugadores_dreamteam(lista_dreamteam, True)
             indice = input("Ingrese el indice del jugador a mostrar las estadísticas. ")
-            print(mostrar_estadisticas(lista_dreamteam, int(indice)))
+            mensaje = mostrar_estadisticas(lista_dreamteam, int(indice))
+            print(mensaje)
+                                 
+            
+        case 3:
+            if not indice:
+                print("Primero debes seleccionar a un jugador, ingresando desde la opción 2.")
+            else:    
+                pregunta = input("Quiere exportar las estadísticas del jugador en un .csv? (si/no)")
+                if re.match(r"si|no",pregunta,re.I):
+                    guardar_csv_jugador_stats(lista_dreamteam,int(indice), generar_path_jugador(lista_dreamteam,int(indice),path_stats_jugadores))
+    input("Apriete una tecla para continuar...")
+                
+        
